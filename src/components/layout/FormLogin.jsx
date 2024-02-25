680396c3870c1ad1a1f6c109146c5493078da760
BiaTrivillin <biatrivillin.code@gmail.com>
2024-02-25 12:56:12 -0300

    created signup page validation functions and show password btn function

--- src/components/layout/FormLogin.jsx
+++ src/components/layout/FormLogin.jsx
@@ -1,10 +1,30 @@
 import '../../styles/forms.css'
 import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEnvelope } from "react-icons/fa";
 import Button from './Button';
+import { useState } from 'react';
 
 
 function FormLogin() {
+
+  const [showPasswordType, setShowPasswordType] = useState('password')
+
+  const [showPasswordSvg, setShowPasswordSvg] = useState('input_svg')
+  
+  const [hidePasswordSvg, setHidePasswordSvg] = useState('input_svg show')
+
+
+  const showPassword = () => {
+    if(showPasswordType !== 'text') setShowPasswordType('text')
+    else setShowPasswordType('password')
+
+    if(showPasswordSvg !== 'input_svg show') setShowPasswordSvg('input_svg show')
+    else setShowPasswordSvg('input_svg')
+
+    if(hidePasswordSvg !== 'input_svg') setHidePasswordSvg('input_svg')
+    else setHidePasswordSvg('input_svg show')
+  }
 
     return (
       <div className='form_container'>
@@ -16,8 +36,8 @@ function FormLogin() {
           </div>
           <div className='input_text_container'>
             <label htmlFor='password_login'>Senha</label>
-            <button><FaRegEye className='input_svg' id='show_pass_btn' /></button>
-            <input className="input_text" id="password_login" required='required' type="password" placeholder="Insira sua senha utilizando apenas números"></input>
+            <button onClick={showPassword} id='show_pass_btn'><FaRegEye className={showPasswordSvg} /><FaRegEyeSlash className={hidePasswordSvg} /></button>
+            <input className="input_text" id="password_login" required='required' type={showPasswordType} placeholder="Insira sua senha utilizando apenas números"></input>
           </div>
           <div className='input_radio_container'>
             <input id="radio_login" type="radio"></input>
--- src/components/layout/FormSignUp.jsx
+++ src/components/layout/FormSignUp.jsx
@@ -1,39 +1,164 @@
 import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 import Button from './Button';
+import { useState } from 'react';
+import '../../styles/forms.css'
 
 function FormSignUp() {
 
+  const [showPasswordTypePass, setShowPasswordTypePass] = useState('password')
+  const [showPasswordTypeConfirm, setShowPasswordTypeConfirm] = useState('password')
+
+  const [showPasswordSvg, setShowPasswordSvg] = useState('input_svg')
+  const [showPasswordConfirmSvg, setShowPasswordConfirmSvg] = useState('input_svg')
+  
+  const [hidePasswordSvg, setHidePasswordSvg] = useState('input_svg show')
+  const [hidePasswordConfirmSvg, setHidePasswordConfirmSvg] = useState('input_svg show')
+
+  const passwordArrays = { 
+    'passwordType': [ showPasswordTypePass, showPasswordTypeConfirm],
+    'setPasswordType': [ setShowPasswordTypePass, setShowPasswordTypeConfirm],
+    'showPasswordSvg': [ showPasswordSvg, showPasswordConfirmSvg],
+    'setShowPasswordSvg': [ setShowPasswordSvg, setShowPasswordConfirmSvg],
+    'hidePasswordSvg': [ hidePasswordSvg, hidePasswordConfirmSvg],
+    'setHidePasswordSvg': [ setHidePasswordSvg, setHidePasswordConfirmSvg],
+  };
+
+  const showPassword = (indice) => {
+    if(passwordArrays.passwordType[indice] !== 'text') passwordArrays.setPasswordType[indice]('text')
+    else passwordArrays.setPasswordType[indice]('password')
+
+    if(passwordArrays.showPasswordSvg[indice] !== 'input_svg show') passwordArrays.setShowPasswordSvg[indice]('input_svg show')
+    else passwordArrays.setShowPasswordSvg[indice]('input_svg')
+
+    if(passwordArrays.hidePasswordSvg[indice] !== 'input_svg') passwordArrays.setHidePasswordSvg[indice]('input_svg')
+    else passwordArrays.setHidePasswordSvg[indice]('input_svg show')
+  }
+
+
+  const [emailInformation, setEmailInformation] = useState('')
+  const [passwordInformation, setPasswordInformation] = useState('')
+  const [passConfirmInformation, setPassConfirmInformation] = useState('')
+  const [termsInformation, setTermsInformation] = useState('')
+  const [nameInformation, setNameInformation] = useState('')
+
+
+  const [emailError, setEmailError] = useState('error')
+  const [passwordError, setPasswordError] = useState('error')
+  const [passCornfirmError, setPassConfirmError] = useState('error')
+  const [termsError, setTermsError] = useState('error')
+  const [nameError, setNameError] = useState('error')
+
+
+  const nameValidation = (event) => {
+    setNameInformation(event.target.value)
+
+    let name = event.target.value
+
+    const checkEmail = (name) => {
+      let emailPattern =  /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+      return emailPattern.test(name); 
+    }
+
+    if(checkEmail(name) !== true) {
+      setNameError('error show')
+    } else {
+      setNameError('error')
+    }
+    
+  }
+
+  const emailValidation = (event) => {
+    setEmailInformation(event.target.value)
+
+    let email = event.target.value
+
+    const checkEmail = (email) => {
+      let emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+      return emailPattern.test(email); 
+    }
+
+    if(checkEmail(email) !== true) {
+      setEmailError('error show')
+    } else {
+      setEmailError('error')
+    }
+    
+  }
+
+  const passwordValidation = (event) => {
+    setPasswordInformation(event.target.value)
+
+    let password = event.target.value
+
+    const checkPassword = (password) => {
+      let PasswordPattern =  /^[0-9]+$/;
+      return PasswordPattern.test(password); 
+    }
+
+    if(checkPassword(password) !== true) {
+      setPasswordError('error show')
+    } else {
+      setPasswordError('error')
+    }
+  }
+
+  const passConfirmValidation = (event) => {
+    setPassConfirmInformation(event.target.value)
+
+    let passwordConfim = event.target.value 
+
+    if(passwordConfim !== passwordInformation) {
+      setPassConfirmError('error show')
+    } else {
+      setPassConfirmError('error')
+    }
+  }
+
+  const termsValidation = (event) => {
+    setTermsInformation(event.target.checked)
+
+    let terms = event.target.checked
+
+    if(terms !== true) {
+      setTermsError('error show')
+    } else {
+      setTermsError('error')
+    }
+  }
+  
   return (
     <div className='form_container'>
       <fieldset className="inputs_container">
         <div className='input_text_container'>
           <label htmlFor='name_signup'>Nome</label>
-          <input className="input_text" id="name_signup" required='required' type="text" placeholder="Insira seu nome"></input>
+          <input value={nameInformation} onChange={nameValidation} className="input_text" id="name_signup" required='required' type="text" placeholder="Insira seu nome"></input>
         </div>
+        <span className={nameError}>Seu nome deve conter apenas letras</span>
         <div className='input_text_container'>
           <label htmlFor='email_signup'>E-mail</label>
-          <input className="input_text" id="email_signup" required='required' type="text" placeholder="Insira seu e-mail"></input>
+          <input value={emailInformation} onChange={emailValidation} className="input_text" id="email_signup" required='required' type="text" placeholder="Insira seu e-mail"></input>
         </div>
-        <span>Esse e-mail é inválido</span>
+        <span className={emailError}>Esse e-mail é inválido</span>
         <div className='input_text_container'>
           <label htmlFor='password_signup'>Senha</label>
-          <button><FaRegEye className='input_svg' /></button>
-          <input className="input_text" id="password_signup" required='required' type="password" placeholder="Insira sua senha utilizando apenas números"></input>
+          <button onClick={() => {showPassword(0)}}><FaRegEye className={showPasswordSvg} /><FaRegEyeSlash className={hidePasswordSvg} /></button>
+          <input value={passwordInformation} onChange={passwordValidation} className="input_text" id="password_signup" required='required' type={showPasswordTypePass} placeholder="Insira sua senha utilizando apenas números"></input>
         </div>
-        <span>Apenas números são permitidos nesse campo</span>
+        <span className={passwordError}>Apenas números são permitidos nesse campo</span>
         <div className='input_text_container'>
           <label htmlFor='password_confirm_signup'>Confirmar senha</label>
-          <button><FaRegEye className='input_svg' /></button>
-          <input className="input_text" id="password_confirm_signup" required='required' type="password" placeholder="Repita a senha criada anteriormente"></input>
+          <button onClick={() => {showPassword(1)}}><FaRegEye className={showPasswordConfirmSvg} /><FaRegEyeSlash className={hidePasswordConfirmSvg} /></button>
+          <input value={passConfirmInformation} onChange={passConfirmValidation} className="input_text" id="password_confirm_signup" required='required' type={showPasswordTypeConfirm} placeholder="Repita a senha criada anteriormente"></input>
         </div>
-        <span>As senhas não correspondem</span>
+        <span className={passCornfirmError}>As senhas não correspondem</span>
         <div className='input_radio_container'>
-          <input id="radio_terms" type="radio"></input>
+          <input value={termsInformation} onChange={termsValidation} id="radio_terms" type="checkbox"></input>
           <label htmlFor="radio_terms">Li e Concordo com os <a>Termos e Condições</a></label>
         </div>
-        <span>Você deve concordar com os termos para continuar</span>
+        <span className={termsError}>Você deve concordar com os termos para continuar</span>
         <div className='input_radio_container'>
-          <input id="radio_remember_information" type="radio"></input>
+          <input id="radio_remember_information" type="checkbox"></input>
           <label htmlFor="radio_remember_information">Lembrar e-mail e senha</label>
         </div>
       </fieldset>
