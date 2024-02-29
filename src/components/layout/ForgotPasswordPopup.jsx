47cca00db101844193619374ce4042d7600af658
BiaTrivillin <biatrivillin.code@gmail.com>
2024-02-29 00:08:57 -0300

    created email validation for forgot pass popup

--- src/components/layout/ForgotPasswordPopup.jsx
+++ src/components/layout/ForgotPasswordPopup.jsx
@@ -2,14 +2,39 @@ import '../../styles/popups.css';
 import Button from './Button';
 import { FaRegEnvelope } from "react-icons/fa";
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 
 function ForgotPasswordPopup ({showForgotPassPopup}) {
 
+    const [emailValid, setEmailValid] = useState(false);
+
+    const [emailError, setEmailError] = useState('error');
+
+    const emailValidation = (e) => {
+    
+        let email = e.target.value;
+    
+        const checkEmail = (email) => {
+          let emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+          return emailPattern.test(email); 
+        }
+    
+        if(!checkEmail(email) && email !== '') {
+          setEmailValid(false);
+          setEmailError('error show');
+        } else {
+          setEmailValid(true);
+          setEmailError('error');
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        showForgotPassPopup();
+        
+        if(emailValid) {
+            showForgotPassPopup();
+        }
     }
 
     return (
@@ -19,8 +44,9 @@ function ForgotPasswordPopup ({showForgotPassPopup}) {
             <div className='input_text_container popup_input_text_container'>
                 <label htmlFor='email_forgot_pass'>E-mail</label>
                 <FaRegEnvelope className='input_svg' />
-                <input className="input_text" id="email_forgot_pass" type="text" placeholder="Insira seu e-mail"></input>
+                <input onChange={emailValidation} className="input_text" id="email_forgot_pass" type="text" placeholder="Insira seu e-mail"></input>
             </div>
+            <span className={emailError}>Esse e-mail é inválido</span>
             <Button value="Enviar" id="forgot_pass_btn" />
             <div className='cancel_btn_container'>
                 <button type='button' onClick={() => showForgotPassPopup()}>Cancelar</button>
